a3fab92949511def8d086743df4451f06c4be93c
TriosMerve <merve@trios.com.tr>
2025-07-27 10:47:38 +0300

    mobil ve responsive duzenlemeler

--- public/assets/front/js/app.js
+++ public/assets/front/js/app.js
@@ -1,3 +1,13 @@
 window.onload = (event) => {
   document.querySelector(".preloader").remove();
 };
+
+const body = document.querySelector("body");
+const navMenu = document.querySelector(".navbar");
+const navButton = document.querySelector(".navbarButton");
+function handleClickMenu(){
+  this.classList.toggle("active");
+  navMenu.classList.toggle("opened");
+  body.classList.toggle("opened");
+}
+navButton.addEventListener("click", handleClickMenu);
