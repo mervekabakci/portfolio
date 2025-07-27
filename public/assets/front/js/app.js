08edcf5dd44f38af81fdf9ba48f6bae30aed9aab
TriosMerve <merve@trios.com.tr>
2025-07-27 11:54:31 +0300

    menu fixed, tıklayınca kaydırma, active  ve uzmanlıklara kayan yazı eklendi.

--- public/assets/front/js/app.js
+++ public/assets/front/js/app.js
@@ -1,13 +1,52 @@
-window.onload = (event) => {
+//Window load
+window.onload = () => {
   document.querySelector(".preloader").remove();
 };
+//Window load end
 
+//Mobile Menu
 const body = document.querySelector("body");
 const navMenu = document.querySelector(".navbar");
 const navButton = document.querySelector(".navbarButton");
-function handleClickMenu(){
+function handleClickMenu() {
   this.classList.toggle("active");
   navMenu.classList.toggle("opened");
   body.classList.toggle("opened");
 }
 navButton.addEventListener("click", handleClickMenu);
+//Mobile Menu End
+
+//window scroll
+let lastScrollY = window.scrollY;
+function handleScroll() {
+  //window scroll header fixed
+  const header = document.querySelector("header");
+  const headerH = header.offsetHeight;
+
+  window.scrollY > headerH
+    ? header.classList.add("fixed")
+    : header.classList.remove("fixed");
+  //window scroll header fixed end
+
+  console.log(headerH);
+}
+window.addEventListener("scroll", handleScroll);
+//window scroll end
+
+const anchorLinks = document.querySelectorAll(".anchorLink");
+
+anchorLinks.forEach((anchorLink) => {
+  anchorLink.addEventListener("click", (event) => {
+    anchorLinks.forEach(link => link.classList.remove("active"));
+    anchorLink.classList.add("active");
+
+    const href = anchorLink.getAttribute("href");
+    const targetElem = document.querySelector(href);
+
+    if (targetElem) {
+      event.preventDefault();
+      targetElem.scrollIntoView({behavior: "smooth"});
+    }
+    console.log(href);
+  });
+});
